092e7e18052e2e34aa8894d9662e68fb67fb66b6
Evgenij Pashkeciv <epaskevic4@gmail.com>
2025-02-02 16:39:27 +0300

    initial commit

---
+++ src/test/java/com/ansysan/coffeemarket/CoffeeMarketApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ansysan.coffeemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class CoffeeMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
