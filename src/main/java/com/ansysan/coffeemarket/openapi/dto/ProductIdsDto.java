624c61a43f08ede41abb8b72f079e8b29a1907a2
Evgenij Pashkeciv <epaskevic4@gmail.com>
2025-02-06 21:35:10 +0300

    adder product

--- src/main/java/com/ansysan/coffeemarket/openapi/dto/ProductIdsDto.java
+++ src/main/java/com/ansysan/coffeemarket/openapi/dto/ProductIdsDto.java
@@ -1,4 +1,11 @@
 package com.ansysan.coffeemarket.openapi.dto;
 
+import lombok.Data;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
 public class ProductIdsDto {
+    private List<UUID> ProductIds;
 }
--- src/main/java/com/ansysan/coffeemarket/openapi/dto/ProductInfoDto.java
+++ src/main/java/com/ansysan/coffeemarket/openapi/dto/ProductInfoDto.java
@@ -1,4 +1,11 @@
 package com.ansysan.coffeemarket.openapi.dto;
 
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class ProductInfoDto {
+    private UUID Id;
+    private String ProductFileUrl;
 }
